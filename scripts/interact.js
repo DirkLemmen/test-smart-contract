2438dc8012218a7a01e43ab478aa6e00e81b802b
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2022-02-23 14:47:00 +0100

    Initial commit

---
+++ scripts/interact.js
@@ -0,0 +1,26 @@
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const {ethers} = require("hardhat");
+const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+
+// provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="rinkeby", API_KEY);
+// signer - you
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// contract instance
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main () {
+    const message = await helloWorldContract.message();
+    console.log("The message is: " + message);
+}
+ 
+main()
+.then(() => process.exit(0))
+.catch(error => {
+  console.error(error);
+  process.exit(1);
+});
